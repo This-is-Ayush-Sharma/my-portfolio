1bb1fc3601606a245aaf73a3cd2cf47318b9d904
Ayush Sharma <ThisIsAyush79@gmail.com>
2022-01-15 02:18:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <title>Ayush Sharma/Porflio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="scroll-button">
+        <a href="#home"><i class="fas fa-arrow-up"></i></a>
+    </div>
+    <nav>
+        <div class="navbar">
+            <div class="logo">
+                <a href="#">Portfolio</a>
+            </div>
+            <ul class="menu">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#contact">Contacts</a></li>
+                <div class="cancel-btn">
+                    <i class="fas fa-times"></i>
+                </div>
+            </ul>
+            <div class="media-icons">
+                <a target="__blank" href="https://www.facebook.com/profile.php?id=100006639769081"><i class="fab fa-facebook-f"></i></a>
+                <a target="__blank" href="https://www.instagram.com/_ayush.1717/"><i class="fab fa-instagram"></i></a>
+                <a target="__blank" href="https://github.com/This-is-Ayush-Sharma"><i class="fab fa-github"></i></a>
+            </div>
+        </div>
+        <div class="menu-btn">
+            <i class="fas fa-bars"></i>
+        </div>
+    </nav>
+    <!-- home section -->
+    <section class="home" id="home">
+        <div class="home-content">
+            <div class="text">
+                <div class="text-one">Hello,</div>
+                <div class="text-two">I'm Ayush Sharma</div>
+                <div class="text-three">Backend Developer</div>
+                <div class="text-four">From India</div>
+            </div>
+            <div class="button">
+                <button>Hire Me</button>
+            </div>
+        </div>
+    </section>
+    <!-- about section  -->
+    <section class="about" id="about">
+        <div class="content">
+            <div class="title"><span>About Me</span></div>
+            <div class="about-details">
+                <div class="left">
+                    <img src="./images/aboutMe.jpg" alt="Ayush Sharma">
+                </div>
+                <div class="right">
+                    <div class="topic">
+                        Coding Is My Passion
+                    </div>
+                    <p>My name is Ayush Sharma.<br>I hail from Rourkela ,Odisha<br>
+                        Currently pursuing B.Tech from GIET University, Gunupur in th domain of
+                        Computer Science and Engineering(C.S.E).
+                        I am back-end Developer.<br>
+                        I am keen in learning new things on a daily basis. 
+                    </p>
+                    <div class="button">
+                        <a href="./resume.pdf"><button>Download CV</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- my sill section  -->
+    <section class="skills" id="skills">
+        <div class="content">
+            <div class="title"><span>My Skills</span></div>
+            <div class="skills-details">
+                <div class="text">
+                    <div class="topic">
+                        Skills Reflects Our Knowledge
+                    </div>
+                    <p>After a very long hard work came up with all this skill set.</p>
+                    <div class="experiance">
+                        <div class="num">2</div>
+                        <div class="exp">Years Of <br> Experience</div>
+                    </div>    
+                </div>
+                <div class="boxes">
+                    <div class="box">
+                        <div class="topic">HTML</div>
+                        <div class="per">90%</div>
+                    </div>
+
+                    <div class="box">
+                        <div class="topic">CSS</div>
+                        <div class="per">70%</div>
+                    </div>
+
+                    <div class="box">
+                        <div class="topic">Javascript</div>
+                        <div class="per">75%</div>
+                    </div>
+
+                    <div class="box">
+                        <div class="topic">Java</div>
+                        <div class="per">80%</div>
+                    </div>
+
+                    <div class="box">
+                        <div class="topic">C</div>
+                        <div class="per">85%</div>
+                    </div>
+
+                    <div class="box">
+                        <div class="topic">PHP</div>
+                        <div class="per">70%</div>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- my services  -->
+    <section class="services" id="services">
+        <div class="content">
+            <div class="title"><span>My Services</span></div>
+            <div class="boxes">
+                <div class="box">
+                    <div class="icon">
+                        <i class="fas fa-desktop"></i>
+                    </div>
+                    <div class="topic">Web Development</div>
+                    <p>Had done lots of projects in web development.
+                        don't forget to check my github for some exciting projects
+                    </p>
+                </div>
+                <div class="box">
+                    <div class="icon">
+                        <i class="fas fa-mobile"></i>
+                    </div>
+                    <div class="topic">Android Development</div>
+                    <p>Just a beginner in Android Development.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- contact me section  -->
+    <section class="contact" id="contact">
+        <div class="content">
+            <div class="title"><span>Contact Me</span></div>
+            <div class="text">
+                <div class="topic">Have any Projects?</div>
+                <p>Do contact me for Collaboration or Help</p>
+                <div class="button">
+                   <a href="https://wa.me/919348935747"> <button><i class="fab fa-whatsapp space"></i>Let's Chat</button></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section  -->
+    <footer>
+        <div class="text"><span>Created By Ayush Sharma | &#169; All Rights Reserved</span></div>
+    </footer>
+    <script src="script.js"></script>
+</body>
+
+</html> 
